7fc46172a43909f05e81bb03204f2746eadbcded
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-08-29 13:19:17 -0600

    add: new liveLeaderboard

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from '../header/header.component';
 import { LiveMapComponent } from "../live-map/live-map.component";
 import { LiveStatisticsComponent } from "../live-statistics/live-statistics.component";
 import { AlertsComponent } from "../alerts/alerts.component";
+import { LiveLeaderboardComponent } from "../../componentes/live-leaderboard/live-leaderboard.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, HeaderComponent, LiveMapComponent, LiveStatisticsComponent, AlertsComponent],
+  imports: [SidebarComponent, HeaderComponent, LiveMapComponent, LiveStatisticsComponent, AlertsComponent, LiveLeaderboardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/componentes/live-leaderboard/live-leaderboard.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+export interface Pilots {
+  position: number,
+  name: string, 
+  team: string, 
+  gap: string, 
+  penalties: number, 
+  status: string, 
+  color: string,
+}
+
+const pilotsData: Pilots[] = [
+  {position: 1, name: 'Jenny', team: "unknow", gap: 'Leader', penalties: 0, status: '', color: '#F315C3'},
+  {position: 2, name: 'Jacob', team: "unknow", gap: '', penalties: 1, status: '5', color: '#D6F2FB'},
+  {position: 3, name: 'Larry', team: "unknow", gap: '', penalties: 0, status: '', color: '#F31515'},
+  {position: 4, name: 'Mary', team: "unknow", gap: '', penalties: 0, status: '', color: '#3715F3'},  
+];
+
+@Component({
+  selector: 'app-live-leaderboard',
+  standalone: true,
+  imports: [MatTableModule, CommonModule],
+  templateUrl: './live-leaderboard.component.html',
+  styleUrl: './live-leaderboard.component.css'
+})
+export class LiveLeaderboardComponent {
+  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-team', 'demo-gap', 'demo-penalties', "demo-status"];
+  dataSource = pilotsData
+}
