bb33a1bd79b7f02d4264fdcd98ccd4007b78dc58
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-08-29 00:33:43 -0600

    add Live statitics

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { HeaderComponent } from '../header/header.component';
 import { LiveMapComponent } from "../live-map/live-map.component";
+import { LiveStatisticsComponent } from "../live-statistics/live-statistics.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, HeaderComponent, LiveMapComponent],
+  imports: [SidebarComponent, HeaderComponent, LiveMapComponent, LiveStatisticsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/componentes/live-statistics/live-statistics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-statistics',
+  standalone: true,
+  imports: [],
+  templateUrl: './live-statistics.component.html',
+  styleUrl: './live-statistics.component.css'
+})
+export class LiveStatisticsComponent {
+
+}
