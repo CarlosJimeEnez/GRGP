e621b8e43f0f100cd8e100e22fdc28bac0f47c45
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-09-02 14:01:40 -0600

    new render

--- src/app/componentes/live-map/live-map.component.ts
+++ src/app/componentes/live-map/live-map.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MapPoints} from "./vectors"
+import * as THREE from 'three'
+import * as YUKA from 'yuka'
+
 
 @Component({
   selector: 'app-live-map',
@@ -8,6 +12,121 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   templateUrl: './live-map.component.html',
   styleUrl: './live-map.component.css'
 })
-export class LiveMapComponent {
+export class LiveMapComponent implements OnInit, AfterViewInit {
+  renderer!: any
+  entityManager: any
+  scene: any
+  camera: any
+  time: any
+  offseX: number = 3
+  offseY: number = -4
+  scaleFactor: number = 0.03; 
+  
+  @ViewChild('mapContainer', { static: false }) mapContainer!: ElementRef;
 
+  constructor(){
+    this.entityManager = new YUKA.EntityManager();
+    this.scene = new THREE.Scene();
+    this.time = new YUKA.Time();
+  }
+
+  ngAfterViewInit(): void {
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      (this.mapContainer.nativeElement.clientWidth) / (this.mapContainer.nativeElement.clientHeight),
+      0.1,
+      1000
+    );
+
+    //Render
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setSize(this.mapContainer.nativeElement.clientWidth, this.mapContainer.nativeElement.clientHeight);
+    this.renderer.setClearColor(0xEFF8FB);
+    this.camera.position.set(0,20,0);
+    this.camera.lookAt(this.scene.position);
+
+
+    // Crear la geometría de la esfera
+    const radius = 0.3; // Radio de la esfera, ajusta según sea necesario
+    const widthSegments = 32; // Segmentos horizontales
+    const heightSegments = 32; // Segmentos verticales
+
+    const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+
+    // Crear el material para la esfera
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Cambia el color si lo deseas
+
+    // Crear la malla de la esfera
+    const sphere = new THREE.Mesh(sphereGeometry, material);
+    sphere.matrixAutoUpdate = false
+    // Añadir la esfera a la escena
+    this.scene.add(sphere);
+
+    // Configurar la posición inicial de la esfera si es necesario
+    // sphere.position.set(10, 0, 0); // Cambia las coordenadas según sea necesario
+
+    const vehicleAgent = new YUKA.Vehicle();
+    vehicleAgent.setRenderComponent(sphere, sync);
+    function sync(entity: any, renderComponent: any) {
+      renderComponent.matrix.copy(entity.worldMatrix);
+    }
+
+    // Points
+    const path = new YUKA.Path(); 
+    MapPoints.forEach((point: any) => {
+        path.add( new YUKA.Vector3((point[0] * this.scaleFactor) - this.offseX, 0, (point[1] * this.scaleFactor) + this.offseY));
+    })
+
+    path.loop = true;
+    vehicleAgent.position.copy(path.current());
+
+    //vehicleAgent.maxSpeed = 3;
+    const followPathBehavior = new YUKA.FollowPathBehavior(path, 0.5);
+    vehicleAgent.steering.add(followPathBehavior);
+
+    const onPathBehavior = new YUKA.OnPathBehavior(path);
+    onPathBehavior.radius = 1;
+    vehicleAgent.steering.add(onPathBehavior);
+  
+    this.entityManager.add(vehicleAgent);
+
+    const position: any = [];
+    MapPoints.forEach((element: any) => {
+      position.push((element[0]*this.scaleFactor) - this.offseX, 0, (element[1] * this.scaleFactor) + this.offseY)
+      console.log(position)
+    });
+
+    const lineGeometry = new THREE.BufferGeometry();
+    lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+
+    const lineMaterial = new THREE.LineBasicMaterial({color: 0x000000});
+    const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+    this.scene.add(lines);
+
+    this.renderer.setAnimationLoop(() => this.animate());
+
+    this.mapContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.onResize();
+  }
+
+  
+  
+  ngOnInit() {
+    
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event) {
+    this.camera.aspect = this.mapContainer.nativeElement.clientWidth / this.mapContainer.nativeElement.clientHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(this.mapContainer.nativeElement.clientWidth, this.mapContainer.nativeElement.clientHeight);
+    
+  }
+
+  animate(): void {
+    const delta = this.time.update().getDelta();
+    this.entityManager.update(delta);
+    this.renderer.render(this.scene, this.camera);
+  }
 }
