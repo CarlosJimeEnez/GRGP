c9f9f0a7b286021387d122004711ec96c66aafb3
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-09-05 22:44:13 -0600

    ...

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
+  imports: [FontAwesomeModule],
+  template: `
+  <div class="container">
+    <h1>as</h1>
+    
+  </div>
+  `, 
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
